eb2ea0163be0c3485c3a9ffe811aa80b28017529
liy14 <liy14@rose-hulman.edu>
2018-05-24 13:38:50 -0400

    FINISH

--- src/problem2.py
+++ src/problem2.py
@@ -2,12 +2,13 @@
 Final exam, problem 2.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.  May 2018.
+         their colleagues and Yi Li.  May 2018.
 
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import time
 import testing_helper
+
 
 
 def main():
@@ -19,9 +20,9 @@ def main():
     print()
     print('Un-comment the calls in MAIN one by one')
     print(' to run the testing code as you complete the TODOs.')
-    # run_test_problem2a()
-    # run_test_problem2b()
-    # run_test_problem2c()
+    run_test_problem2a()
+    run_test_problem2b()
+    run_test_problem2c()
 
 
 def is_prime(n):
@@ -170,9 +171,14 @@ def problem2a(x, sequence):
       :type sequence:  [int]
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
+    new = []
+    for k in range(len(sequence)):
+        if sequence[k] > x:
+            new = new + [sequence[k]]
+    return new
 
 
 def run_test_problem2b():
@@ -250,9 +256,16 @@ def problem2b(sequence):
       :type sequence  [int]
     """
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
+    max = 0
+    for k in range(len(sequence)):
+        if sequence[k] < 0:
+            sequence[k] = -sequence[k]
+        if sequence[max] < sequence[k]:
+            max = k
+    return max
 
 
 def run_test_problem2c():
@@ -393,9 +406,14 @@ def problem2c(x):
       :type x:    int
     """
     # -------------------------------------------------------------------------
-    # TODO: 4. Implement and test this function.
+    # DONE: 4. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
+    while True:
+        if is_prime(x) is True:
+            if is_prime(sum_of_digits(x)) is True:
+                return x
+        x = x + 1
 
 
 ###############################################################################
